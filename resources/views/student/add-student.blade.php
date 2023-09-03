454f006555d24175c400d0a99e196f8c19aee360
Mohamed Hedi Sfaxi <medhedisfaxi2@gmail.com>
2023-09-03 01:44:08 +0100

    First commit

---
+++ resources/views/student/add-student.blade.php
@@ -0,0 +1,163 @@
+
+@extends('layouts.master')
+@section('content')
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+
+            <div class="page-header">
+                <div class="row align-items-center">
+                    <div class="col-sm-12">
+                        <div class="page-sub-header">
+                            <h3 class="page-title">Add Students</h3>
+                            <ul class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{ route('student/add/page') }}">Student</a></li>
+                                <li class="breadcrumb-item active">Add Students</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- message --}}
+            {!! Toastr::message() !!}
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card comman-shadow">
+                        <div class="card-body">
+                            <form action="{{ route('student/add/save') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h5 class="form-title student-info">Student Information
+                                            <span>
+                                                <a href="javascript:;"><i class="feather-more-vertical"></i></a>
+                                            </span>
+                                        </h5>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Nom <span class="login-danger">*</span></label>
+                                            <input type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" placeholder="Entrer le nom" value="{{ old('first_name') }}">
+                                            @error('first_name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Prenom <span class="login-danger">*</span></label>
+                                            <input type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" placeholder="Entrer le prenom" value="{{ old('last_name') }}">
+                                            @error('last_name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms calendar-icon">
+                                            <label>Date de naissance <span class="login-danger">*</span></label>
+                                            <input class="form-control datetimepicker @error('date_of_birth') is-invalid @enderror" name="date_of_birth" type="text" placeholder="DD-MM-YYYY" value="{{ old('date_of_birth') }}">
+                                            @error('date_of_birth')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>CIN </label>
+                                            <input class="form-control type="text" name="roll" placeholder="Enter CIN Number" value="{{ old('roll') }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Payement Tranche 2 <span class="login-danger">*</span></label>
+                                            <select class="form-control select @error('religion') is-invalid @enderror" name="religion">
+                                                <option selected disabled>Selectionner Payement </option>
+                                                <option value="Hindu" {{ old('religion') == 'Hindu' ? "selected" :""}}>oui</option>
+                                                <option value="Christian" {{ old('religion') == 'Christian' ? "selected" :""}}>non</option>
+                                                {{-- <option value="Others" {{ old('religion') == 'Others' ? "selected" :""}}>Others</option> --}}
+                                            </select>
+                                            @error('religion')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>E-Mail <span class="login-danger">*</span></label>
+                                            <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" placeholder="Entrer l'email" value="{{ old('email') }}">
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Groupe <span class="login-danger">*</span></label>
+                                            <select class="form-control select @error('class') is-invalid @enderror" name="class">
+                                                <option selected disabled>Selectionner le groupe </option>
+                                                <option value="12" {{ old('class') == '12' ? "selected" :""}}>G1</option>
+                                                <option value="11" {{ old('class') == '11' ? "selected" :""}}>G2</option>
+                                                <option value="10" {{ old('class') == '10' ? "selected" :""}}>G3</option>
+                                            </select>
+                                            @error('class')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Formation <span class="login-danger">*</span></label>
+                                            <select class="form-control select @error('section') is-invalid @enderror" name="section">
+                                                <option selected disabled>Selectionner la formation </option>
+                                                <option value="A" {{ old('section') == 'A' ? "selected" :""}}>Diagnostic mécanique auto et moto</option>
+                                                <option value="B" {{ old('section') == 'B' ? "selected" :""}}>Réparation et programmation de calculateur auto </option>
+                                                <option value="C" {{ old('section') == 'C' ? "selected" :""}}>Carrosserie, tôlerie et peinture</option>
+                                                <option value="D" {{ old('section') == 'D' ? "selected" :""}}>Mécanique poids lourds</option>
+                                                <option value="E" {{ old('section') == 'E' ? "selected" :""}}>Technique de soudage</option>
+                                                <option value="F" {{ old('section') == 'F' ? "selected" :""}}>Plomberie chaud et froid</option>
+                                                <option value="G" {{ old('section') == 'G' ? "selected" :""}}>Electricité industriel et bâtiment</option>
+                                                <option value="H" {{ old('section') == 'H' ? "selected" :""}}>Camera de surveillance</option>
+                                            </select>
+                                            @error('section')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Telephone </label>
+                                            <input class="form-control @error('phone_number') is-invalid @enderror" type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1').replace(/^0[^.]/, '0');" name="phone_number" placeholder="Entrer num telephone" value="{{ old('phone_number') }}">
+                                            @error('phone_number')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="student-submit">
+                                            <button type="submit" class="btn btn-primary">Ajouter</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
